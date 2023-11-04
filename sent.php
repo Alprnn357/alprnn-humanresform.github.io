0bb4f7d90efeeb23dcd7ce7118f41c4cda03d95f
Alprnn357 <37711793+Alprnn357@users.noreply.github.com>
2023-11-04 15:05:46 +0300

    Add files via upload

---
+++ sent.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css"/>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>İnsan Kaynakları Formu</title>
+    </head>
+    <body>
+        <header class="kutu">
+            <h1>İnsan Kaynakları Formu</h1>
+        </header>
+        <main>
+            <section class="kutu">
+                <p>Formunuz başarıyla gönderildi. Teşekkürler</p><br>
+                <a href="index.php">Geri dönmek için buraya tıklayınız.</a>
+                
+            </section>
+        </main>
+    </body>
+</html>
